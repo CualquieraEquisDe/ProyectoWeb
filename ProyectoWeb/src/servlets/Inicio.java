bbc9ed1a0ca4ff607eb1ef49b61d40536e9e820d
Sebastian Vargas <31361213+SebastianVargas98@users.noreply.github.com>
2017-10-30 21:49:07 -0500

    First Commit

---
+++ ProyectoWeb/src/servlets/Inicio.java
@@ -0,0 +1,77 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Inicio
+ */
+@WebServlet("/Inicio")
+public class Inicio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Inicio() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	/** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        /*DBContactos conDb = new DBContactos();
+        try {
+            ResultSet res = conDb.getContactos();
+            
+            request.getSession().setAttribute("contactos", res);
+            response.sendRedirect("index.jsp");
+        }catch(Exception e){
+            
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Inicio</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Error at " + e.toString() + "</h1>");
+            out.println("<p>seguimiento: "+conDb.getMensaje()+"</p>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        }finally {            
+            out.close();
+        }*/
+    }
+
+}
